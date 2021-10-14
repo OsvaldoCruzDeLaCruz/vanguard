64143e37562c5e9b1f1c2555ae26e787fdee1bb5
Osvaldo Cruz <osvaldocruzdelacruz2001@gmail.com>
2021-10-13 23:23:39 -0500

    Cambio en el color de un tono de amarillo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const btnSwitch = document.querySelector("#switch");
+const iconMode = document.querySelector("#icon-mode");
+const logo = document.querySelector('#logo');
+
+
 const fecha = new Date()
 // const hora = fecha.getHours()
 const hora = fecha.getHours()
@@ -6,23 +10,27 @@ const hora = fecha.getHours()
 let changeModeDark = ()=>{
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('activate');
-    localStorage.setItem('dark-mode', 'True');
+    logo.src = "/images/image.png"
+    localStorage.setItem('dark-mode', 'True'); 
+    
 }
 
 let changeModeLigth = () =>{
     document.body.classList.toggle('Ligth');
     btnSwitch.classList.toggle('Ligth');
     localStorage.setItem('dark-mode', 'False');
+    logo.src = "/images/logo_preview_rev_1.png"
 }
 
 
 if(localStorage.getItem('dark-mode') == 'True'){
     changeModeDark()
 }
+
 else if(localStorage.getItem('dark-mode') == 'False' && hora >= 19 || localStorage.getItem('dark-mode') == 'False' && hora >= 1 && hora < 7 ){    
     setTimeout(()=>{
         window.confirm("Ya es de noche \n ¿Deseas activar en modo obscuro?") ? changeModeDark() : changeModeLigth()
-    },2000)
+    },2500)
     
 }
 
@@ -34,6 +42,7 @@ btnSwitch.addEventListener('click', () =>{
     }        
     else{
         localStorage.setItem('dark-mode', 'False');
+        changeModeLigth()
     }    
 })    
 
